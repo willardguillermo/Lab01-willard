9e563a79777cd318d5c726189aab9323d859e947
guillermo willard <willardguillermo@gmail.com>
2026-08-19 16:51:50 -0500

    Proyecto inicial: Hola Mundo con Jetpack Compose

---
+++ app/src/main/java/com/willard/laboratorio00/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.willard.laboratorio00
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.willard.laboratorio00.ui.theme.Laboratorio00Theme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Laboratorio00Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Column(modifier=modifier) {
+        Text(text= "Hola, soy $name", fontSize = 24.sp)
+        Text(text= "Curso: Programacion en moviles",)
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Laboratorio00Theme {
+        Greeting("Guillermo Willard")
+    }
+}
